e43c6a8721158aa22ea11e024dd08490307a35a8
Will Concepcion <willnba22@gmail.com>
2019-09-30 11:06:49 -0400

    qui3

---
+++ quiz3.py
@@ -0,0 +1,24 @@
+class FizzBuzzer:
+    def __init__(self, start):
+        self.start = 0
+        self.number = number
+        self.next = 0
+
+    def next():
+        self.next += 1
+        return str(self.number)
+
+    def fizzbuzz(self):
+        if self.number % 3 == 0 and self.number % 5 == 0:
+            print("fizzbuzz")
+        elif self.number % 5 == 0:
+            print("buzz")
+        elif self.number % 3 == 0:
+            print("fizz")
+        else:
+            print(self.number)
+    
+    buzzer = fizzbuzz(5)
+    print(buzzer.next())
+    print(buzzer.next())
+    print(buzzer.next())
